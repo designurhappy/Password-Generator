61662e6c5806fe9758becf1c1ad613f3a44aa6a6
Moo Point <beachgal0105@gmail.com>
2020-11-11 23:33:02 -0800

    generate password functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,13 @@ const generateBtn = document.querySelector('#generate');
 const copyBtn = document.querySelector('#copy');
 let passwordText = document.querySelector('#password');
 
+//character arrays
+let lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+let uppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+let numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
+let specialCharacters = ['!', '"', '#', '$', '%', '&', '(', ')', '*', '+', '?', '@', ',', '-', '.', '/', ':', ';', '<', '>', '=', '[', ']', '\\', '^', '_', '`', '{', '}', '|', '~'];
 
+let randomPassword = [];
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -17,6 +23,16 @@ if (generateBtnprompt > 0) {
  window.alert("Click OK to confirm including special characters.");
 }
 
+//generate password
+function generatePassword(arr, number) {
+  let newArray = arr.flat();
+  let newPassword = [];
+  for (let i = 0; i < number; i++) {
+      let randomIndex = Math.floor(Math.random() * newArray.length - 1) + 1;
+      newPassword.push(newArray[randomIndex]);
+  }
+  return newPassword.join('');
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -28,4 +44,4 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
+}
